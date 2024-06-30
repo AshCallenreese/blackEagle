acf66dfe269de3fdc95a7f26b615e408144147c7
AshCallenreese <vincenzoaffatato929@gmail.com>
2024-06-30 11:38:36 +0200

    Add components

--- blackeaglefrontend/src/App.tsx
+++ blackeaglefrontend/src/App.tsx
@@ -1,26 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import About from "./component/About";
+import Home from "./component/Home";
+import Create from "./component/Create";
+import {Route, Routes} from 'react-router-dom';
+import Navbar from './component/Navbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+= "inheret"
 
 export default App;
